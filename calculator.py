d4e5423e762e79a09c5ad5a4e00fc49d52f9034e
Kush-Uniyal <66991456+Kush-Uniyal@users.noreply.github.com>
2020-06-18 13:48:51 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+print("choose option")
+print("1.add")
+print("2.subtract")
+print("3.multiply")
+print("4.divide")
+choice=input("enter to choose option 1/2/3/4 :")
+num1=int(input("enter no. 1 :"))
+num2=int(input("enter no. 2 :"))
+if choice=='1':
+         print("num1+num2" ,"=",add(num1,num2))
+elif choice=='2':
+         print("num1-num2" ,"=",subtract(num1,num2))
+elif choice=='3':
+         print("num1*num2" ,"=",multiply(num1,num2))
+elif choice=='2':
+         print("num1/num2 ","=",divide(num1,num2))
+else:
+         print("invalid input")
+         
